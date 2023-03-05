70471cf8273e1da47c0436ec0fc7b134c42f67d5
Oleksandr Serebriakov <sealex1977@gmail.com>
2023-03-05 03:59:59 +0200

    update sigma_hw-final folder

--- sigma_hw-final/src/App.js
+++ sigma_hw-final/src/App.js
@@ -1,12 +1,15 @@
 import './assets/scss/style.scss';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import AboutUs from './components/AboutUs/AboutUs';
+
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
+      <AboutUs />
       <div className="container">
         <h1>heelo</h1>
       </div>
---
+++ sigma_hw-final/src/components/AboutUs/AboutUs.js
@@ -0,0 +1,9 @@
+function AboutUs() {
+    return (
+        <div className="about-us__wrapper">
+            it is about us!!
+        </div>
+    )
+}
+
+export default AboutUs;
