f24015408873619aa06b8030b8a726c77170e9a8
Oleksandr Serebriakov <sealex1977@gmail.com>
2023-03-09 13:04:31 +0200

    update what we do section

--- sigma_hw-final/src/components/WhatWeDo/WhatWeDo.js
+++ sigma_hw-final/src/components/WhatWeDo/WhatWeDo.js
@@ -6,8 +6,54 @@ function WhatWeDo() {
         <section className="what-we-do__container">
             <div className="what-we-do__subtitle">what we do</div>
             <div className="what-we-do__title">Our Service</div>
+            <div className="what-we-do__buttons">
+                <div className="what-we-do__btn">
+                    <span>all</span>
+                </div>
+                <div className="what-we-do__btn">
+                    <span>interior design</span>
+                </div>
+                <div className="what-we-do__btn">
+                    <span>architecture</span>
+                </div>
+                <div className="what-we-do__btn">
+                    <span>planning</span>
+                </div>
+            </div>
             <div className="what-we-do__content">
-
+                <div className="what-we-do__service">
+                    <div className="what-we-do__service-icon icon1"></div>
+                    <div className="what-we-do__service-info">
+                        <div className="what-we-do__service-title">Interioir Design 1</div>
+                        <div className="what-we-do__service-text">
+                            Lorem ipsum dolor amet consectetur
+                            adipiscing elit sed eiusmod tempor
+                            incididunt ut labore.
+                        </div>
+                    </div>
+                </div>
+                <div className="what-we-do__service decored">
+                    <div className="what-we-do__service-icon icon2"></div>
+                    <div className="what-we-do__service-info">
+                        <div className="what-we-do__service-title decored">Architecture 1</div>
+                        <div className="what-we-do__service-text decored">
+                            Lorem ipsum dolor amet consectetur
+                            adipiscing elit sed eiusmod tempor
+                            incididunt ut labore.
+                        </div>
+                    </div>
+                </div>
+                <div className="what-we-do__service">
+                    <div className="what-we-do__service-icon icon3"></div>
+                    <div className="what-we-do__service-info">
+                        <div className="what-we-do__service-title">Planning 1</div>
+                        <div className="what-we-do__service-text">
+                            Lorem ipsum dolor amet consectetur
+                            adipiscing elit sed eiusmod tempor
+                            incididunt ut labore.
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
